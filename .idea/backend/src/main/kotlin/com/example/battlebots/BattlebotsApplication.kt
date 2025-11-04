c6c45f3cf5c0da970b8a6b228ca441b694bc4d2e
ZézonhoDuJob <168040148+Zanellaxxx@users.noreply.github.com>
2025-11-04 20:13:09 -0300

    Initial commit

---
+++ .idea/backend/src/main/kotlin/com/example/battlebots/BattlebotsApplication.kt
@@ -0,0 +1,12 @@
+package com.example.battlebots
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BattlebotsApplication
+
+fun main(args: Array<String>) {
+	runApplication<BattlebotsApplication>(*args)
+}
+aaaaaa
---
+++ .idea/backend/src/test/kotlin/com/example/battlebots/BattlebotsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.battlebots
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BattlebotsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
